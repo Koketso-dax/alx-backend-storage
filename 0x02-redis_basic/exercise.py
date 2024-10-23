8895a3cf27ec3a11dc32674134d1db5bac33b334
Koketso-dax <lanchaster.k@gmail.com>
2024-10-23 16:59:12 +0200

    Added decorators

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,24 +2,48 @@
 """
 Cache class that stores an instance of redis.Redis()
 """
-from typing import Union
 import uuid
 import redis
+from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Decorator to count method calls """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function to count method calls """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def callhistory(method: Callable) -> Callable:
+    """ Decorator to store method call history """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function to store method call history """
+        self._redis.rpush(method.__qualname__ + ":calls", str(args))
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
     """ Cache class using redis store """
     def __init__(self) -> None:
-        self._redis = redis.Redis() # default: localhost:6379
+        self._redis = redis.Redis()  # default: localhost:6379
         self._redis.flushdb(True)
 
+    @callhistory
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Stores value in redis and returns key """
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
         return data_key
 
-    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: callable = None) -> Union[str, bytes, int, float]:
         """ Retrieves value from redis using key """
         value = self._redis.get(key)
         if fn:
