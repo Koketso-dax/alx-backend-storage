075385d2621aa148ee57351b837ec696a2cc084b
Koketso-dax <lanchaster.k@gmail.com>
2024-10-23 23:58:07 +0200

    made cache page awaitable

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,24 +3,28 @@
 Caching request module
 """
 import redis
+import redis.typing
 import requests
 from functools import wraps
-from typing import Callable
+from typing import Awaitable, Callable, Union
 
 
 def track_get_page(fn: Callable) -> Callable:
     """ Decorator for get_page
     """
     @wraps(fn)
-    def wrapper(url: str) -> str:
+    async def wrapper(url: str) -> str:
         """ Wrapper that:
             - check whether a url's data is cached
             - tracks how many times get_page is called
         """
         client = redis.Redis()
-        client.incr(f'count: {url}')
-        if client.exists(url):
-            return client.get(url).decode('utf-8')
+        client.incr(f'count:{url}')
+        cached_page: Union[Awaitable[bytes], bytes, None] = client.get(url)
+        if cached_page is not None:
+            if isinstance(cached_page, Awaitable):
+                cached_page = await cached_page
+            return cached_page.decode('utf-8')
         response = fn(url)
         client.set(url, response, 10)
         return response
